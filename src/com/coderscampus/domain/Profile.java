f466a6bd3c2fda9e3ffed9891f98ead8245a3758
trevorgittest <trevor@javavideotutorials.net>
2023-05-12 16:29:01 -0400

    Added comments about branches

--- src/com/coderscampus/domain/Profile.java
+++ src/com/coderscampus/domain/Profile.java
@@ -2,6 +2,21 @@ package com.coderscampus.domain;
 
 import java.time.LocalDate;
 
+
+/**
+ * Branches
+ * main/master -> represents the code that's actually live on the internet (ie coderscampus.com)
+ * UAT (not typical)
+ * QA
+ * Development
+ * 
+ *
+ * Environments
+ * Production -> main/master
+ * UAT (not typical)
+ * QA
+ * Dev
+ */
 public class Profile {
 	private Integer id;
 	private String profileName;
